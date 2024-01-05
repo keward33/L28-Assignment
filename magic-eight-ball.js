96fc38720010e4847095fbaf692b4443ffe200b9
keward33 <keward3@my.waketech.edu>
2024-01-04 21:22:27 -0500

    Update magic-eight-ball.js

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -1,2 +1,28 @@
 // Put your JavaScript code in this file
 
+const answers = [
+  "Yes",
+  "No",
+  "Maybe",
+  "Not likely",
+  "Cannot predict now",
+  "Could be",
+];
+
+function displayAnswer() {
+  let index = Math.floor(Math.random() * answers.length);
+  document.getElementById("circle").innerHTML = answers[index];
+  document.getElementById("circle").style.display = "inline-block";
+}
+
+ball.addEventListener("mousedown", function () {
+  if (document.getElementById("question").value == "") {
+    alert("Please ask a question!");
+  } else {
+    displayAnswer();
+  }
+});
+
+reset.addEventListener("click", function () {
+  document.getElementById("circle").style.display = "none";
+});
